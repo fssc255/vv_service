827bc49539bf1a8bc5c1047d0b2c5b301bfe40b3
HiroMuraki <419734782@qq.com>
2025-11-17 01:02:42 +0800

    update

--- start-service.sh
+++ start-service.sh
@@ -2,5 +2,4 @@
 
 export HF_ENDPOINT=https://hf-mirror.com
 
-# cd src && uv run uvicorn app:app --host "127.0.0.1" --port 6590
-cd src && uv run app.py
+cd src && uv run uvicorn app:app --host "127.0.0.1" --port 6590
